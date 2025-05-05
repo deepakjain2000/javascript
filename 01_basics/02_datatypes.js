7f1fd55cda557fa8dd06fb9e58521afe5bb41d09
Deepak jain <168455347+deepakjain2000@users.noreply.github.com>
2025-05-05 15:25:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as new version
+
+let name = "Deepak"
+let age = 20
+let state;
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
